aaa7db1abf0a27e81893a1424e707e45d5484f5d
Catmiao9527 <92568458+Catmiao9527@users.noreply.github.com>
2021-12-26 09:55:41 +0800

    Update DeleteBook.java

--- Exam/DeleteBook.java
+++ Exam/DeleteBook.java
@@ -1,5 +1,3 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -7,13 +5,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
